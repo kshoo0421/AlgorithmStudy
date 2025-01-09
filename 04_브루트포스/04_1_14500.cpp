8b7af7154673ad31b24810e4286de347733c1aab
kshoo0421 <shmd02@naver.com>
2025-01-09 19:37:30 +0900

    4일차 필수 문제 업데이트

--- 04_브루트포스/04_1_14500.cpp
+++ 04_브루트포스/04_1_14500.cpp
@@ -1,41 +1,48 @@
+/* [풀이]
+1. 모든 경우의 수를 다 찾아야한다.
+2. 한 칸씩 확장해서 4칸이 될 때의 합을 확인(ㅡ, ㅣ, ㄴ, ㄱ, ㅁ)
+3. 그 외 모양 확인(ㅏ, ㅗ, ㅓ, ㅜ)
+4. 모든 칸 확인 후 그 중 가장 큰 값 확인
+*/
+
 #include <bits/stdc++.h>
-#define X second
 #define Y first
+#define X second
 using namespace std;
 int N, M, mx = 0;
 vector<vector<int>> space;
 vector<pair<int, int>> tetris;
 
-void FindMax(int y, int x, int idx) {
-	if (y < 0 || y >= N || x < 0 || x >= M) return;
-	for (auto vii : tetris) if (vii.Y == y && vii.X == x) return;
+void FindMax(int y, int x, int idx) { // 현재 위치(y, x). 몇 번째 칸인지 확인(0~3)
+	if (y < 0 || y >= N || x < 0 || x >= M) return; // 범위 밖
+	for (auto pii : tetris) if (pii.Y == y && pii.X == x) return; // 이미 탐색한 칸이면 탈출
 
-	tetris.emplace_back(make_pair(y, x));
-	if (idx == 3) {
-		int sum = 0;
-		for (auto vii : tetris) sum += space[vii.Y][vii.X];
-		if (sum > mx) mx = sum;
+	tetris.push_back({ y, x }); // 현재 칸 추가
+	if (idx == 3) { // 4칸째
+		int sum = 0; // 전체 합 계산
+		for (auto vii : tetris) sum += space[vii.Y][vii.X]; 
+		if (sum > mx) mx = sum; // 최대치보다 크면 최대치 없데이트
 	}
 	else {
-		FindMax(y + 1, x, idx + 1);
-		FindMax(y - 1, x, idx + 1);
-		FindMax(y, x + 1, idx + 1);
-		FindMax(y, x - 1, idx + 1);
+		FindMax(y + 1, x, idx + 1); // 상
+		FindMax(y - 1, x, idx + 1); // 하
+		FindMax(y, x + 1, idx + 1); // 좌
+		FindMax(y, x - 1, idx + 1); // 우
 	}
-	tetris.pop_back();
+	tetris.pop_back(); // 현재 칸 제거
 }
 
 void FindTValue(int y, int x) {
 	if ((y == 0 || y == N - 1) && (x == 0 || x == M - 1)) return;
 	int sum = 0;
-	if (y == 0) sum = space[y][x] + space[y][x - 1] + space[y][x + 1] + space[y + 1][x];
-	else if (x == 0) sum = space[y][x] + space[y - 1][x] + space[y + 1][x] + space[y][1];
-	else if (y == N - 1) sum = space[y][x] + space[y][x - 1] + space[y][x + 1] + space[y - 1][x];
-	else if (x == M - 1) sum = space[y][x] + space[y][x - 1] + space[y + 1][x] + space[y - 1][x];
+	if (y == 0) sum = space[y][x] + space[y][x - 1] + space[y][x + 1] + space[y + 1][x]; // ㅜ
+	else if (x == 0) sum = space[y][x] + space[y - 1][x] + space[y + 1][x] + space[y][1];  // ㅏ
+	else if (y == N - 1) sum = space[y][x] + space[y][x - 1] + space[y][x + 1] + space[y - 1][x]; // ㅓ
+	else if (x == M - 1) sum = space[y][x] + space[y][x - 1] + space[y + 1][x] + space[y - 1][x]; // ㅗ
 	else {
-		sum = space[y][x] + space[y][x - 1] + space[y][x + 1] + space[y + 1][x] + space[y - 1][x];
-		int mn = min(min(space[y + 1][x], space[y - 1][x]), min(space[y][x + 1], space[y][x - 1]));
-		sum -= mn;
+		sum = space[y][x] + space[y][x - 1] + space[y][x + 1] + space[y + 1][x] + space[y - 1][x]; // +
+		int mn = min(min(space[y + 1][x], space[y - 1][x]), min(space[y][x + 1], space[y][x - 1])); // 상하좌우 중 가장 작은 것
+		sum -= mn; // 빼기
 	}
 	if (mx < sum) mx = sum;
 }
@@ -48,8 +55,8 @@ int main()
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			FindMax(i, j, 0);
-			FindTValue(i, j);
+			FindMax(i, j, 0); // ㅡ, ㅣ, ㄴ, ㄱ, ㅁ 등
+			FindTValue(i, j); // ㅓ, ㅗ, ㅜ, ㅏ
 		}
 	}
 	cout << mx;
--- 04_브루트포스/04_2_01107.cpp
+++ 04_브루트포스/04_2_01107.cpp
@@ -1,62 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool btn_err[10];
 
-bool is_num_possible(int n)
-{
-	if (n < 10) return !btn_err[n];
-	while (n)
-	{
-		if (btn_err[n % 10]) return false;
+int N, M;
+bool btnErr[10];
+
+// 버튼으로 바로 누를 수 있는지 확인
+bool IsNumPossible(int n) {
+	if (n < 10) return !btnErr[n]; // 1자리수면 버튼 고장났는지 반환
+	while (n) { // 각 자릿수 확인
+		// 고장난 버튼 있으면 불가
+		if (btnErr[n % 10]) return false;
 		n /= 10;
 	}
-	return true;
+	return true; // 없으면 버튼으로 이동 가능
 }
 
 int main()
 {
 	ios::sync_with_stdio(0), cin.tie(0);
-	bool err_done = false;
-	int n, m, u, d, tmp, ans, a1, a2, a3;
-	cin >> n >> m;
+	bool errDone = false;
+	cin >> N >> M;
 	
-	for(int i = 0; i < m; i++)
-	{ 
-		cin >> tmp;
-		btn_err[tmp] = true;
+	for(int i = 0; i < M; i++) { 
+		int input;
+		cin >> input;
+		btnErr[input] = true; // 고장 체크
 	}
 	
-	if (m == 10)
-	{
-		cout << abs(n - 100);
+	if (M == 10 || (M == 9 && btnErr[0] == false)) { // 모두 고장나거나 0만 멀쩡하면 ( +, - )로만 움직여야함.
+		cout << abs(N - 100);
 		return 0;
 	}
 
-	u = n, d = n;
+	int up = N, down = N;
 	
-	while (!is_num_possible(u))
-	{
-		if (m == 9 && btn_err[0] == false)
-		{
-			u = 100;
-			break;
-		}
-		u++;
-	}
-	while (!is_num_possible(d))	d--;
-	
-	a1 = abs(u - n);
-	tmp = u;
-	if (u == 0) a1++;
-	while (tmp)	tmp /= 10, a1++;
+	while (!IsNumPossible(up)) up++; // 한 채널씩 올려봄
+	while (!IsNumPossible(down)) down--; // 버튼 사용 가능할 때까지 내림
 
-	if (d < 0) d = 100;
-	a2 = abs(n - d);
-	tmp = d;
-	if (d == 0) a2++;
-	while (tmp)	tmp /= 10, a2++;
+	int a1 = abs(up - N); // 기본 횟수 : 목표치와 up의 차이
+	int tmp = up;
+	if (up == 0) a1++; // 
+	while (tmp)	tmp /= 10, a1++; // 숫자 버튼 누르는 횟수 추가
 
-	ans = min(a1, a2);
-	ans = min(ans, abs(n - 100));
-	cout << ans;
+	if (down < 0) down = 100; // 나올 수 없는 값이면 기본값
+	int a2 = abs(N - down); // N과 down의 차이
+	tmp = down;
+	if (down == 0) a2++;
+	while (tmp)	tmp /= 10, a2++;  // 버튼 누르는 횟수
+
+	cout << min(min(a1, a2), abs(N - 100));
 }
