7087e3362ad1fe9bbba45995ba3f3cd987d1610a
kshoo0421 <shmd02@naver.com>
2025-01-10 17:43:21 +0900

    스도쿠 추가

--- 05_백트래킹/05_4_02580.cpp
+++ 05_백트래킹/05_4_02580.cpp
@@ -1,66 +1,69 @@
+/* [풀이]
+1. 빈칸 모두를 한 번에 채워야 함. 
+그러므로 빈 칸을 모두 vector에 넣고 백트래킹 시도
+2. 각 칸에 가능한 숫자를 확인하고, 넣을 수 있다면 다른 빈 칸 채우기 시도
+3. 모두 다 채워졌다면 종료
+*/
+
 #include <bits/stdc++.h>
-#define X first
-#define Y second
+#define Y first
+#define X second
 using namespace std;
 
-int space[9][9];
-vector<pair<int, int>> blanks;
+vector<vector<int>> grid(9, vector<int>(9)); // 스도쿠판
+vector<pair<int, int>> blanks; // 빈 칸 좌료
 
-bool IsValid(int x, int y, int num) {
-    // 라인
-    for (int i = 0; i < 9; i++) {
-        if (space[x][i] == num || space[i][y] == num) {
-            return false;
+bool IsValid(int y, int x, int num) { // 이 위치에 해당 숫자가 가능한지 여부 확인
+    for (int i = 0; i < 9; i++) { // 가로/세로 줄 중에
+        if (grid[y][i] == num || grid[i][x] == num) { // 이 숫자가 있다면
+            return false; // 이 숫자 불가
         }
     }
 
-    // 33
-    x = (x / 3) * 3;
-    y = (y / 3) * 3;
-    for (int i = x; i < x + 3; i++) {
-        for (int j = y; j < y + 3; j++) {
-            if (space[i][j] == num) {
-                return false;
+    y = (y / 3) * 3; // 이 3x3칸의 y 시작 지점
+    x = (x / 3) * 3; // 이 3x3칸의 x 시작 지점
+    for (int cy = y; cy < y + 3; cy++) { // 3칸씩 이동
+        for (int cx = x; cx < x + 3; cx++) { // 3칸씩 이동
+            if (grid[cy][cx] == num) { // 이미 원하는 숫자가 있다면
+                return false; // 이 숫자 불가
             }
         }
     }
-
-    // 통과
-    return true;
+    return true;    // 통과
 }
 
-bool BackTracking(int idx) {
-    if (idx == blanks.size()) {
-        return true;
+bool BackTracking(int idx) { // 빈 칸 백트래킹
+    if (idx == blanks.size()) { // 모든 칸에 맞는 숫자를 찾았다면
+        return true; // 성공
     }
-    int x = blanks[idx].X;
-    int y = blanks[idx].Y;
+    int cy = blanks[idx].Y; // 빈 칸의 y 좌표
+    int cx = blanks[idx].X; // 빈 칸의 x 좌료
 
     for (int num = 1; num <= 9; num++) {
-        if (IsValid(x, y, num)) {
-            space[x][y] = num;
+        if (IsValid(cy, cx, num)) {
+            grid[cy][cx] = num;
             if (BackTracking(idx + 1)) {
                 return true;
             }
-            space[x][y] = 0;
+            grid[cy][cx] = 0;
         }
     }
-    return false;
+    return false; // 실패
 }
 
 int main() {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            cin >> space[i][j];
-            if (space[i][j] == 0) {
+            cin >> grid[i][j];
+            if (grid[i][j] == 0) {
                 blanks.push_back({ i, j });
             }
         }
     }
     BackTracking(0);
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 9; i++) { // 출력
         for (int j = 0; j < 9; j++) {
-            cout << space[i][j] << " ";
+            cout << grid[i][j] << " ";
         }
         cout << "\n";
     }
