5ed0b5640d7cf9e82fd5cde383c79a8382a6017f
kshoo0421 <shmd02@google.com>
2025-01-13 20:09:56 +0900

    주석 2개 추가

--- 06_해시테이블/06_1_04195.cpp
+++ 06_해시테이블/06_1_04195.cpp
@@ -1,3 +1,9 @@
+/* [풀이]
+1. 이름->인덱스 형식의 map 활용, Union Find 알고리즘 사용
+2. 새로운 사람이 나타날 때마다 parent와 size를 합쳐주기
+3. SetUnion을 하면 두 집단의 size를 합쳐주기
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,12 +18,14 @@ int main()
 		map<string, int> nameIdx;
 
 		cin >> F;
-		vector<int> parent, size;
+		vector<int> parent, size; // 부모, 현재의 크기
+		// 부모 찾기 함수
 		function<int(int)> FindParent = [&](int a) -> int {
 			if (a == parent[a]) return a;
 			return parent[a] = FindParent(parent[a]);
 			};
 
+		// 유니온 설정
 		function<void(int, int)> SetUnion = [&](int x, int y) -> void {
 			int rootX = FindParent(x);
 			int rootY = FindParent(y);
@@ -27,16 +35,16 @@ int main()
 					swap(rootX, rootY);
 				}
 				parent[rootY] = rootX;
-				size[rootX] += size[rootY];
+				size[rootX] += size[rootY]; // 크기 통합
 			}};
 
 		while (F--) {
 			string name1, name2;
 			cin >> name1 >> name2;
-			if (nameIdx.find(name1) == nameIdx.end()) {
-				parent.push_back(idx);
-				size.push_back(1);
-				nameIdx[name1] = idx++;
+			if (nameIdx.find(name1) == nameIdx.end()) { // 인덱스가 없다면
+				parent.push_back(idx); // 부모 : 인덱스에 맞게 추가
+				size.push_back(1); // 크기 1 추가
+				nameIdx[name1] = idx++; // 인덱스 등록
 			}
 			if (nameIdx.find(name2) == nameIdx.end()) {
 				parent.push_back(idx);
@@ -45,7 +53,7 @@ int main()
 			}
 
 			int idx1 = nameIdx[name1], idx2 = nameIdx[name2];
-			SetUnion(idx1, idx2);
+			SetUnion(idx1, idx2); // 두 인덱스를 하나의 유니온으로 합치기
 			cout << size[FindParent(idx1)] << "\n";
 		}
 	}
--- 06_해시테이블/06_4_01351.cpp
+++ 06_해시테이블/06_4_01351.cpp
@@ -1,3 +1,7 @@
+/* [풀이]
+1. 재귀 형식으로 필요한 A[i]만 생성
+2. map으로 해당 인덱스의 값이 있는지 확인
+*/
 #include <bits/stdc++.h>
 #define LL long long
 using namespace std;
@@ -10,15 +14,15 @@ void FindAi(LL i) {
 	else {
 		LL iP = i / P;
 		LL iQ = i / Q;
-		if (A.find(iP) == A.end()) {
-			FindAi(iP);
+		if (A.find(iP) == A.end()) { // A[i/P]가 없다면
+			FindAi(iP);	// 재귀로 찾기
 		}
 
-		if (A.find(iQ) == A.end()) {
-			FindAi(iQ);
+		if (A.find(iQ) == A.end()) { // A[i/Q]가 없다면
+			FindAi(iQ);	// 재귀로 찾기
 		}
 
-		A[i] = A[iP] + A[iQ];
+		A[i] = A[iP] + A[iQ]; // 합쳐서 구하기
 	}
 }
 
