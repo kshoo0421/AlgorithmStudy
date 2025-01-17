af1135ca63da121ee9a35800e410a0d4851a6548
kshoo0421 <shmd02@naver.com>
2025-01-17 21:09:04 +0900

    10-1

--- 10_MST/10_1_01197.cpp
+++ 10_MST/10_1_01197.cpp
@@ -1,35 +1,42 @@
+/* [풀이]
+1. 정석적인 MST. 유니온 파인드를 활용한 크루스칼 알고리즘 구현
+*/
 #include <bits/stdc++.h>
 using namespace std;
+
+vector<int> parent, ranks;
+vector<vector<int>> edges;
+
+int FindParent(int a) {
+	if (a == parent[a]) return a;
+	return parent[a] = FindParent(parent[a]);
+}
+
+void SetUnion(int x, int y) {
+	int rootX = FindParent(x);
+	int rootY = FindParent(y);
+
+	if (rootX != rootY) {
+		if (ranks[rootX] < ranks[rootY]) parent[rootX] = rootY;
+		else if (ranks[rootX] > ranks[rootY]) parent[rootY] = rootX;
+		else {
+			parent[rootY] = rootX;
+			ranks[rootX]++;
+		}
+	}
+}
 
 int main()
 {
 	ios::sync_with_stdio(0), cin.tie(0);
 	int V, E;
 	cin >> V >> E;
-	vector<vector<int>> edges(E, vector<int>(3));
+	edges.assign(E, vector<int>(3));
 	for (auto& edge : edges) cin >> edge[0] >> edge[1] >> edge[2];
 
-	vector<int> parent(V + 1), rank(V + 1, 0);
+	parent.resize(V + 1);
+	ranks.assign(V + 1, 0);
 	for (int i = 1; i <= V; i++) parent[i] = i;
-	
-	function<int(int)> FindParent = [&](int a) -> int {
-		if (a == parent[a]) return a;
-		return parent[a] = FindParent(parent[a]);
-	};
-
-	function<void(int, int)> SetUnion = [&](int x, int y) -> void {
-		int rootX = FindParent(x);
-		int rootY = FindParent(y);
-
-		if (rootX != rootY) {
-			if (rank[rootX] < rank[rootY]) parent[rootX] = rootY;
-			else if (rank[rootX] > rank[rootY]) parent[rootY] = rootX;
-			else {
-				parent[rootY] = rootX;
-				rank[rootX]++;
-			}
-		}
-	};
 
 	sort(edges.begin(), edges.end(), [](vector<int>& v1, vector<int>& v2) {
 		return v1[2] < v2[2];
