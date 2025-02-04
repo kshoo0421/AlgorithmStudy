7ba57c577da8838d977fa351539a7010ac51ad67
kshoo0421 <shmd02@naver.com>
2025-02-04 17:57:47 +0900

    11657

---
+++ 13_최단경로(벨만포드)/13_1_11657.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define U 0
+#define V 1
+#define W 2
+#define MAX 1e9
+#define LL long long
+using namespace std;
+
+int N, M;
+vector<tuple<LL, LL, LL>> edges;
+vector<LL> dist;
+
+bool BellmanFord(int start) {
+    dist.assign(N, MAX);
+    dist[start] = 0;
+
+    for(int i = 0; i < N; i++) {
+        for(auto& [u, v, w] : edges) {
+            if(dist[u] != MAX && dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+            } 
+        }
+    }
+
+    for(auto& [u, v, w] : edges) {
+        if(dist[u] != MAX && dist[v] > dist[u] + w) {
+            return false;
+        } 
+    }
+    return true;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    cin >> N >> M;
+    edges.resize(M);
+    for(auto& [u, v, w] : edges) {
+        cin >> u >> v >> w;
+        u--, v--;
+    }
+
+    if(BellmanFord(0)) {
+        for(int i = 1; i < N; i++) {
+            if(dist[i] == MAX) cout << "-1\n";
+            else cout << dist[i] << "\n";
+        }
+    } 
+    else cout << "-1\n";
+}
