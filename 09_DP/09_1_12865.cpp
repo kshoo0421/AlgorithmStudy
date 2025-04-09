1b88e33e27325f4b7bba4cac2239697746130e8a
kshoo0421 <shmd02@google.com>
2025-04-09 20:25:39 +0900

    9일차 업데이트

--- 09_DP/09_1_12865.cpp
+++ 09_DP/09_1_12865.cpp
@@ -1,15 +1,10 @@
-/* [풀이]
-1. 현재 무게에서 가능한 물건의 가치를 기록한다.
-물건은 하나씩 넣어보며, 무게가 감당 가능할 때 넣는다.
-2. 무게를 하나씩 늘려보며, dp를 작성한다.
-*/
 #include <bits/stdc++.h>
-#define W first // 무게
-#define V second // 가치
+#define W first
+#define V second
 using namespace std;
 
 int memo[101][100001];
-int N, K; // 물품의 수, 버틸 수 있는 무게
+int N, K;
 
 int main()
 {
@@ -17,13 +12,12 @@ int main()
 	cin >> N >> K;
 	vector<pair<int, int>> inputs(N);
 	for (auto& pii : inputs) cin >> pii.W >> pii.V;
-	for (int sum = 1; sum <= K; sum++) { // 무게합이 1~K
-		for (int i = 0; i < N; i++) { // 
-			if (inputs[i].W <= sum) {// 물건의 무게가 현재 가능 무게보다 적다면
-				// 현재 가치 = max(이전 물건까지의 가치, (현재 무게 - 물건 무게)의 가치 + 물건 가치)
+	for (int sum = 1; sum <= K; sum++) {
+		for (int i = 0; i < N; i++) { 
+			if (inputs[i].W <= sum) {
 				memo[i + 1][sum] = max(memo[i][sum], memo[i][sum - inputs[i].W] + inputs[i].V);
 			}
-			else memo[i + 1][sum] = memo[i][sum]; // 아니면 이전 값 그대로
+			else memo[i + 1][sum] = memo[i][sum];
 		}
 	}
 	cout << memo[N][K];
--- 09_DP/09_4_11054.cpp
+++ 09_DP/09_4_11054.cpp
@@ -1,10 +1,3 @@
-/* [풀이]
-1. 수열을 입력 받고 2가지 기준으로 dp용으로 메모한다.
-- left : (왼 -> 오) 현재까지 연속으로 몇 칸 증가했는지
-- right : (오 -> 왼) 현재까지 연속으로 몇 칸 감소했는지
-2. 각 칸을 탐색하며 left + right의 합이 가장 큰 값을 찾는다.
-3. 결괏값을 출력한다. left와 right에 모두 자신이 포함되어 있으니 1을 빼고 출력한다.
-*/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -17,12 +10,12 @@ int main()
 	for (int& i : Ai) cin >> i;
 	for (int i = 0; i < N; i++) {
 		if (left[i] == 0) left[i] = 1;
-		for (int j = i + 1; j < N; j++) { // left
+		for (int j = i + 1; j < N; j++) {
 			if (Ai[i] < Ai[j]) left[j] = max(left[j], left[i] + 1);
 		}
 		int k = N - i - 1;
 		if (right[k] == 0) right[k] = 1;
-		for (int j = k - 1; j >= 0; j--) { // right
+		for (int j = k - 1; j >= 0; j--) {
 			if (Ai[k] < Ai[j]) right[j] = max(right[j], right[k] + 1);
 		}
 	}
